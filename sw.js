4ed1eff9ad4b53ddd7ab7a8d027cfbf6995f62f5
bcf2546 <bcf2546@gmail.com>
2026-06-11 14:50:31 +0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
 // Provides offline support and faster loading after first visit
 // ════════════════════════════════════════════════════════════════
 
-const CACHE_NAME = 'bcf-vault-v18.24';
+const CACHE_NAME = 'bcf-vault-v18.26';
 const CORE_ASSETS = [
   './',
   './index.html',
